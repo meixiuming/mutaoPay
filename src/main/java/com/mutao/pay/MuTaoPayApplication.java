dac00c27e1b0cb30a82ebdc465b3e6cec73c2c5b
18615269525@163.com <18615269525@163.com>
2021-04-27 14:19:24 +0800

    第一次更新

---
+++ src/main/java/com/mutao/pay/MuTaoPayApplication.java
@@ -0,0 +1,13 @@
+package com.mutao.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuTaoPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuTaoPayApplication.class, args);
+	}
+
+}
